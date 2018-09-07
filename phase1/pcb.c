2d4cd16e220c988312c59c5d0317cba024fc5361
Gavin Kyte <gpkyte@gmail.com>
2018-09-07 14:46:17 -0400

    Replace method sigs with e file

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -6,23 +6,9 @@
 #include "../h/types.h"
 #include "../h/const.h"
 
+#include "../e/pcb.e"
+
 pcb_PTR pcbFree_h;
-
-void freePcb (pcb_PTR p);
-pcb_PTR allocPcb ();
-void initPcbs ();
-
-pcb_PTR mkEmptyProcQ ();
-int emptyProcQ (pcb_PTR tp);
-void insertProcQ (pcb_PTR *tp, pcb_PTR p);
-pcb_PTR removeProcQ (pcb_PTR *tp);
-pcb_PTR outProcQ (pcb_PTR *tp, pcb_PTR p);
-pcb_PTR headProcQ (pcb_PTR tp);
-
-int emptyChild (pcb_PTR p);
-void insertChild (pcb_PTR prnt, pcb_PTR p);
-pcb_PTR removeChild (pcb_PTR p);
-pcb_PTR outChild (pcb_PTR p);
 
 /* Insert the element onto the pcbFree list */
 void freePcb (pcb_PTR p) {
