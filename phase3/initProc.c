145735afdcea54fac62b1e73bbcde84e2de1c6c5
Gavin Kyte <gpkyte@gmail.com>
2018-11-21 22:21:09 -0500

    Add template module level documentation

---
+++ phase3/initProc.c
@@ -0,0 +1,28 @@
+/*********************** INITPROC.C ************************
+ * Initialize process
+ *
+ * This module is used to make kernel-level
+ * 	specifications to new processes and the main
+ * 	method, ******, should generally be the
+ * 	starting PC value.
+ *
+ * One major kernal action is 3x sys5 calls.
+ *
+ * AUTHORS: Ploy Sithisakulrat & Gavin Kyte
+ * ADVISOR/CONTRIBUTER: Michael Goldweber
+ * DATE PUBLISHED:
+ ***********************************************************/
+
+#include
+
+/************************ Prototypes ***********************/
+
+
+
+
+/********************* External Methods ********************/
+
+
+
+
+/********************** Helper Methods *********************/
