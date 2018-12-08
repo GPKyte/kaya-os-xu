f49f0e19702f5fbfbc3f7a6e890444151ef9ce26
Gavin Kyte <gpkyte@gmail.com>
2018-12-08 15:51:31 -0500

    Init helper fxns for ADL

--- phase3/adl.c
+++ phase3/adl.c
@@ -15,9 +15,16 @@
 
 #include
 
+delayd_PTR openBeds_h;
+delayd_PTR nextToWake_h;
+
 /************************ Prototypes ***********************/
 
-
+typedef struct delayd_t {
+	delayd_PTR   d_next;
+	int          d_procQ;
+	unsigned int d_wakeTime;
+}
 
 
 /********************* External Methods ********************/
@@ -26,3 +33,38 @@
 
 
 /********************** Helper Methods *********************/
+HIDDEN void freeBed(delayd_t bed) {
+	bed->d_next = openBeds_h;
+	openBeds_h = bed;
+}
+
+HIDDEN delayd_PTR allocBed (void) {
+	if(bedFree_h == NULL) {
+		return (NULL);
+	} else {
+		delayd_PTR cleanBed = bedFree_h;
+		bedFree_h = cleanBed->d_next;
+
+		/* Clean bed */
+		cleanBed->d_next = NULL;
+		cleanBed->d_procQ = mkEmptyProcQ();
+		cleanBed->d_wakeTime = NULL;
+		return (cleanBed);
+	}
+}
+
+/*
+ * searchBeds - an accessor used to find and return the predecessor/proper
+ *		location of the desired semaphore, regardless of whether it exists yet.
+ *
+ * PARAM:	wakeTime - a pointer to a semaphore address.
+ * RETURN:	the predecessor of the given semaphore address in the ASL.
+ */
+HIDDEN delayd_PTR searchBeds (int wakeTime) {
+	delayd_PTR nomad = nextToWake_h;
+
+	while(nomad->d_next->d_wakeTime < wakeTime) {
+		nomad = nomad->d_next;
+	}
+	return (nomad);
+}
